34df71719838997780b0f6caa2ff3fc342850743
Christopher Scott <chriss@microsoft.com>
2022-12-02 17:42:58 -0600

    wip

--- sdk/identity/test-resources-post.ps1
+++ sdk/identity/test-resources-post.ps1
@@ -8,5 +8,5 @@ param (
 Write-Host $DeploymentOutputs['IDENTITY_WEBAPP_USER_DEFINED_IDENTITY']
 Write-Host $DeploymentOutputs['IDENTITY_STORAGE_NAME_2']
 dotnet publish ./sdk/identity/Azure.Identity/tests/WebApp/WebApp.csproj -o ./artifacts/bin/Azure.Identity.Tests/WebApp/Pub
-Compress-Archive -Path ./artifacts/bin/Azure.Identity.Tests/WebApp/Pub* -DestinationPath ./artifacts/bin/Azure.Identity.Tests/WebApp/Pub/package.zip  -Force
+Compress-Archive -Path ./artifacts/bin/Azure.Identity.Tests/WebApp/Pub/* -DestinationPath ./artifacts/bin/Azure.Identity.Tests/WebApp/Pub/package.zip  -Force
 az webapp deploy --resource-group $DeploymentOutputs['IDENTITY_RESOURCE_GROUP'] --name $DeploymentOutputs['IDENTITY_WEBAPP_NAME'] --src-path ./artifacts/bin/Azure.Identity.Tests/WebApp/Pub/package.zip
