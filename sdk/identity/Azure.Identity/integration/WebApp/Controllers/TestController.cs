f8cdb11dd738620d696d9935efe65926d2e5b5e8
Christopher Scott <chriss@microsoft.com>
2022-12-05 14:15:00 -0600

    First iteration of a simple ManagedIdentity integration test

--- sdk/identity/Azure.Identity/integration/WebApp/Controllers/TestController.cs
+++ sdk/identity/Azure.Identity/integration/WebApp/Controllers/TestController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using Azure.Core;
 using Azure.Identity;
 using Azure.Storage.Blobs;
@@ -10,21 +12,14 @@ namespace WebApp.Controllers
     [Route("[controller]")]
     public class TestController : ControllerBase
     {
-        private readonly ILogger<TestController> _logger;
-
-        public TestController(ILogger<TestController> logger)
-        {
-            _logger = logger;
-
-        }
 
         [HttpGet(Name = "GetTest")]
-        public async Task<IActionResult> Get()
+        public IActionResult Get()
         {
             string resourceId = Environment.GetEnvironmentVariable("IDENTITY_WEBAPP_USER_DEFINED_IDENTITY")!;
             string account1 = Environment.GetEnvironmentVariable("IDENTITY_STORAGE_NAME_1")!;
             string account2 = Environment.GetEnvironmentVariable("IDENTITY_STORAGE_NAME_2")!;
-            
+
             var credential1 = new ManagedIdentityCredential();
             var credential2 = new ManagedIdentityCredential(new ResourceIdentifier(resourceId));
             var client1 = new BlobServiceClient(new Uri($"https://{account1}.blob.core.windows.net/"), credential1);
--- sdk/identity/test-resources-post.ps1
+++ sdk/identity/test-resources-post.ps1
@@ -7,6 +7,6 @@ param (
 
 Write-Host $DeploymentOutputs['IDENTITY_WEBAPP_USER_DEFINED_IDENTITY']
 Write-Host $DeploymentOutputs['IDENTITY_STORAGE_NAME_2']
-dotnet publish ./sdk/identity/Azure.Identity/tests/WebApp/WebApp.csproj -o ./artifacts/bin/Azure.Identity.Tests/WebApp/Pub
+dotnet publish ./sdk/identity/Azure.Identity/integration/WebApp/WebApp.csproj -o ./artifacts/bin/Azure.Identity.Tests/WebApp/Pub
 Compress-Archive -Path ./artifacts/bin/Azure.Identity.Tests/WebApp/Pub/* -DestinationPath ./artifacts/bin/Azure.Identity.Tests/WebApp/Pub/package.zip  -Force
 az webapp deploy --resource-group $DeploymentOutputs['IDENTITY_RESOURCE_GROUP'] --name $DeploymentOutputs['IDENTITY_WEBAPP_NAME'] --src-path ./artifacts/bin/Azure.Identity.Tests/WebApp/Pub/package.zip
